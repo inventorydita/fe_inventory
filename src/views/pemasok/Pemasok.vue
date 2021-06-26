5e8afa682ee77f1c2b48f0d7b0cea610a2987b27
triandamai <triannurizkillah@gmail.com>
2021-06-26 19:14:17 +0700

    configure datatable

--- src/views/pemasok/Pemasok.vue
+++ src/views/pemasok/Pemasok.vue
@@ -2,51 +2,24 @@
   <div>
     <CRow>
       <CCol lg="12">
-        <CTableWrapper :items="getShuffledUsersData()">
-          <template #header>
-            <CIcon name="" />
-            <h4>
-              <b>Pemasok</b>
-            </h4>
+        <data-table :items="items" :headers="header" title="Pemasok">
+          <template #tambah>
+            <CButton
+              @click="$router.push({ path: '/master/addpemasok' })"
+              color="primary"
+            >
+              Tambah Data
+            </CButton> </template
+          ><template #search
+            ><CForm inline>
+              <CInput class="ml-12 sm-6" placeholder="Search" size="sm" />
 
-            <div class="container">
-              <div class="row">
-                <div class="col-sm">
-                  <div class="form-group form-actions">
-                    <CButton
-                      @click="$router.push({ path: '/master/addpemasok' })"
-                      color="primary"
-                    >
-                      Tambah Data
-                    </CButton>
-                  </div>
-                </div>
-
-                <div class="col-sm d-flex justify-content-end">
-                  <CForm inline>
-                    <CInput class="ml-12 sm-6" placeholder="Search" size="sm" />
-
-                    <CButton color="outline-success" class="" type="submit"
-                      >Search</CButton
-                    >
-                  </CForm>
-                </div>
-              </div>
-            </div>
-
-            <div class="card-header-actions">
-              <a
-                href="https://coreui.io/vue/docs/components/nav"
-                class="card-header-action"
-                rel="noreferrer noopener"
-                target="_blank"
+              <CButton color="outline-success" class="" type="submit"
+                >Search</CButton
               >
-                <small class="text-muted"></small>
-              </a>
-            </div>
-          </template>
-        </CTableWrapper>
-
+            </CForm></template
+          >
+        </data-table>
         <CPagination align="center" :pages="10" />
       </CCol>
     </CRow>
@@ -54,26 +27,14 @@
 </template>
 
 <script>
-import CTableWrapper from "/src/components/table.vue";
-
 export default {
   name: "Pemasok",
-  components: { CTableWrapper },
-  methods: {
-    shuffleArray(array) {
-      for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        let temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-      }
-      return array;
-    },
-
-    getShuffledUsersData() {
-      return [];
-    },
+  data: () => {
+    return {
+      header: [{ key: "tes", label: "No" }],
+      items: [{ tes: "Hai" }],
+    };
   },
+  methods: {},
 };
 </script>
-
