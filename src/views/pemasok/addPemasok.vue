1be63c3230ce13ebd10f22c18f7fafdffa7d1182
kevinriah <kevinizumi14@gmail.com>
2021-07-29 22:43:01 +0700

    menambahkan coding pada script

--- src/views/pemasok/addPemasok.vue
+++ src/views/pemasok/addPemasok.vue
@@ -8,18 +8,41 @@
 import API from "../../services/api.service";
 
 export default {
+  props: ["body", "isEdit"],
+  data:()=>{
+    return{
+      body:{}
+    }
+  },
+
+  watch: {
+    //pada props:['body'] di awasi disini setiap ada perubahan akan di masukkan ke form
+    body: function (newData) {
+      this.form = newData;
+    },
+  },
+
+
   methods: {
+    submit() {
+      this.$emit("submit", this.form);
+    },
     addPemasok() {
       API.post("pemasokcontroller", {}).then(({status,data}) => {
         if(status == 200 || status == 201){
           if(data.status){
-            this.items = data.data
+           //notifikasi ketika berhasil
+          }else {
+            //gagal
           }
-        
-        
+        }else{
+          //gagal
         }
+      }).catch(()=>{
+        //errpr
       });
     },
+
   },
 };
 </script>
--- src/views/pemasok/editPemasok.vue
+++ src/views/pemasok/editPemasok.vue
@@ -6,16 +6,38 @@
 
 <script>
 export default {
+  props: ["body", "isEdit"],
+  data:()=>{
+    return{
+      body:{}
+    }
+  },
+
+  watch: {
+    //pada props:['body'] di awasi disini setiap ada perubahan akan di masukkan ke form
+    body: function (newData) {
+      this.form = newData;
+    },
+  },
+
   methods: {
+    submit() {
+      this.$emit("submit", this.form);
+    },
+    
     editPemasok() {
       API.put("pemasokcontroller", {}).then((status,data) => {
         if(status == 200 || status == 201){
           if(data.status){
-            this.items = data.data
+            //notifikasi ketika berhasil
+          }else {
+            //gagal
           }
-        
-        
+        }else{
+          //gagal
         }
+      }).catch(()=>{
+        //errpr
       });
     },
   },
