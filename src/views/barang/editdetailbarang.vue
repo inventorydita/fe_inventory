084c63fa28cea504377a59956da7aa9041d82e59
kevinriah <kevinizumi14@gmail.com>
2021-08-08 11:22:50 +0700

    menambahkan notifikasi

--- src/views/barang/editdetailbarang.vue
+++ src/views/barang/editdetailbarang.vue
@@ -16,11 +16,18 @@ export default {
   },
   created() {
     //ambil data by id
-    this.getData()
+    this.getDataDetailBarang()
   },
   methods: {
     //get data by id
-    getData() {
+    getDataDetailBarang() {
+      API.get("masterbarangcontroller").then(({ status, data }) => {
+        if (status == 200 || status == 201) {
+          if (data.status) {
+            this.items = data.data;
+          }
+        }
+      });
 
     },
     onSimpan(data) {
@@ -29,14 +36,44 @@ export default {
         if (status === 200 || status === 201) {
           if (data.status) {
             //notifikasi ketika berhasil
+            this.$notify({
+              // ketika data berhasil dihapus maka muncul notif
+              group: "notif",
+              type: "success",
+              title: "Informasi",
+              text: "Data telah berhasil dihapus",
+            });
+            this.getDataDetailBarang();
+
           } else {
             //gagal
+            this.$notify({
+              group: "notif",
+              type: "error",
+              title: "Perhatian",
+              text: "Data gagal untuk dihapus",
+            });
+
           }
         } else {
           //gagal
+          this.$notify({
+            group: "notif",
+            type: "error",
+            title: "Perhatian",
+            text: "Data gagal untuk dihapus",
+            });
+
         }
       }).catch(() => {
-        //errpr
+        //error
+        this.$notify({
+          group: "notif",
+          type: "error",
+          title: "Perhatian",
+          text: "Data gagal untuk dihapus",
+          });
+          
       });
     },
   },
--- src/views/satuan/addsatuanbarang.vue
+++ src/views/satuan/addsatuanbarang.vue
@@ -22,14 +22,42 @@ export default {
         if(status === 200 || status === 201){
           if(data.status){
              //notifikasi ketika berhasil
+             this.$notify({ // ketika data berhasil dihapus maka muncul notif
+              group: "notif",
+              type: "success",
+              title: "Informasi",
+              text: "Data telah berhasil dihapus",
+            });
+            this.getDataSatuanBarang()
+
           }else {
             //gagal
+            this.$notify({
+              group: "notif",
+              type: "error",
+              title: "Perhatian",
+              text: "Data gagal untuk dihapus",
+            });
           }
         }else{
           //gagal
+          this.$notify({
+              group: "notif",
+              type: "error",
+              title: "Perhatian",
+              text: "Data gagal untuk dihapus",
+            });
+
         }
       }).catch(()=>{
-        //errpr
+        //error
+        this.$notify({
+          group: "notif",
+          type: "error",
+          title: "Perhatian",
+          text: "Data gagal untuk dihapus",
+        });
+        
       });
     },
   },
